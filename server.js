893f6810141f54daa1ccca098984282126f81130
Jay Schwane <jrschwane@uwalumni.com>
2017-04-27 11:33:25 -0500

    enable CORS

--- server.js
+++ server.js
@@ -22,6 +22,11 @@ app.use(bodyParser.urlencoded({ 'extended' : 'true' }));
 app.use(bodyParser.json());
 app.use(bodyParser.json({ type: 'application/vnd.api+json' }));
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 /* ================================ ROUTES ================================= */
 
